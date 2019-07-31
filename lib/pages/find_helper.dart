6756d488b10de5f2dc020741c5e7e72a2349b8bb
xiedong <xiedong11@aliyun.com>
2019-07-31 10:26:27 +0800

    项目页面框架搭建

---
+++ lib/pages/find_helper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FindHelper extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => PageState();
+}
+
+class PageState extends State<FindHelper> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("找帮手"),
+    );
+  }
+}
---
+++ lib/pages/mark_order.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MarkOrder extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => PageState();
+}
+
+class PageState extends State<MarkOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("下订单"),
+    );
+  }
+}
