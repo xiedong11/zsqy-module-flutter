14e7e6d554a0c614ca5affca5188447982d11f2d
xiedong <xiedong11@aliyun.com>
2019-08-03 13:26:05 +0800

    移植数据存储

--- android/app/src/main/java/com/zhuandian/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/zhuandian/flutter_app/MainActivity.java
@@ -1,13 +1,52 @@
 package com.zhuandian.flutter_app;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.zhuandian.flutter_app.util.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    private static final String MESSAGE_CHANNEL = "zhuandian.flutter";
+
+    private String userObjectId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+//        userObjectId = getIntent().getStringExtra("userObjectId");
+        userObjectId ="2f9c77de59";
+
+        initMessageChannl();
+    }
+
+
+    private void initMessageChannl() {
+        new MethodChannel(getFlutterView(), MESSAGE_CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+            @Override
+            public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                if (methodCall.method.equals("getAppConfig")) {
+                    List<String> configs = new ArrayList<>();
+                    configs.add(Constant.APPID);
+                    configs.add(Constant.API_KEY);
+                    result.success(configs);
+                } else if (methodCall.method.equals("getUserObjectId")) {
+                    if (!TextUtils.isEmpty(userObjectId)) {
+                        result.success(userObjectId);
+                    }
+                } else {
+                    result.notImplemented();
+                }
+            }
+        });
+    }
 }
---
+++ lib/utils/user_cache.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_app/entity/user_entity.dart';
+
+class UserCache{
+  static User user;
+}
