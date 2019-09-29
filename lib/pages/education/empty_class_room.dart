8a663f80616fdfdf9c49e9d926b872d4f2b2166e
xiedong <xiedong11@aliyun.com>
2019-09-29 17:27:14 +0800

    升级bmob数据存储库到0.0.13

---
+++ lib/pages/education/empty_class_room.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EmptyClassRoom extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => PageState();
+}
+
+class PageState extends State<EmptyClassRoom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
