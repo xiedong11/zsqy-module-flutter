9db4e99ebf62a9a62a22e1a52916d88b89347d55
xiedong <xiedong11@aliyun.com>
2019-10-18 17:53:20 +0800

    首页添加下拉刷新

--- lib/pages/home/single_day_page.dart
+++ lib/pages/home/single_day_page.dart
@@ -27,11 +27,16 @@ class PageState extends State<SingleDayPage> {
     getBannerEntity();
   }
 
+  Future<void> _onRefresh() async {
+    getBannerEntity();
+  }
+
   void getBannerEntity() async {
     Response result = await Dio().get(Constant.HOME_BANNER_URL);
     HomeConfigEntity homeConfigEntity =
         HomeConfigEntity.fromJson(jsonDecode(result.data));
     this.setState(() {
+      print('-----------'+_bannerList[0].bannerContentUrl);
       _bannerList = homeConfigEntity.banner;
     });
   }
@@ -39,90 +44,96 @@ class PageState extends State<SingleDayPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _weekDay,
+          style: TextStyle(color: Colors.black, fontSize: 23),
+        ),
+        elevation: 0,
+      ),
       body: Container(
         color: Colors.white,
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 10, left: 15, bottom: 10),
-              child: Text(
-                _weekDay,
-                style: TextStyle(color: Colors.black, fontSize: 23),
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              width: double.infinity,
-              height: 150,
-              child: Swiper(
-                itemCount: _bannerList.length > 0 ? _bannerList.length : 0,
-                scale: 0.9,
-                viewportFraction: 0.8,
-                autoplay: true,
-                duration: 2000,
-                pagination: new SwiperPagination(
-                    builder: DotSwiperPaginationBuilder(
+        child: RefreshIndicator(
+          displacement: 40,
+            child: ListView(
+              children: <Widget>[
+                Container(
                   color: Colors.white,
-                  activeColor: Colors.teal,
-                )),
-                controller: new SwiperController(),
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.network(
-                    _bannerList.length > 0
-                        ? _bannerList[index].bannerImgUrl
-                        : "",
-                    fit: BoxFit.cover,
-                  );
-                },
-                onTap: (index) {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => CommonWebPage(
-                          url: _bannerList[index].bannerContentUrl,
-                          title: "")));
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10, left: 15, bottom: 10),
-              child: Row(
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                    width: 5,
-                    child: Container(
-                      color: Colors.teal,
-                    ),
+                  width: double.infinity,
+                  height: 150,
+                  child: Swiper(
+                    itemCount: _bannerList.length > 0 ? _bannerList.length : 0,
+                    scale: 0.9,
+                    viewportFraction: 0.8,
+                    autoplay: true,
+                    duration: 2000,
+                    pagination: new SwiperPagination(
+                        builder: DotSwiperPaginationBuilder(
+                      color: Colors.white,
+                      activeColor: Colors.teal,
+                    )),
+                    controller: new SwiperController(),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Image.network(
+                        _bannerList.length > 0
+                            ? _bannerList[index].bannerImgUrl
+                            : "",
+                        fit: BoxFit.cover,
+                      );
+                    },
+                    onTap: (index) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => CommonWebPage(
+                              url: _bannerList[index].bannerContentUrl,
+                              title: "")));
+                    },
                   ),
-                  SizedBox(width: 5,),
-                  Text(
-                    "校园新鲜事",
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  )
-                ],
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.only(top: 10, left: 15, bottom: 10),
-              child: Row(
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                    width: 5,
-                    child: Container(
-                      color: Colors.deepOrangeAccent,
-                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10, left: 15, bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                        width: 5,
+                        child: Container(
+                          color: Colors.teal,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "校园新鲜事",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      )
+                    ],
                   ),
-                  SizedBox(width: 5,),
-                  Text(
-                    "今日课表",
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  )
-                ],
-              ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10, left: 15, bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                        width: 5,
+                        child: Container(
+                          color: Colors.deepOrangeAccent,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "今日课表",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+            onRefresh: _onRefresh),
       ),
     );
   }
